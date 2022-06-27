a5d254dda7984d5cfe2ae9771f6e5a776cff9347
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-06-27 20:31:34 +0300

    init new project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export const App = () => {
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<></>} />
+    </Routes>
+  </BrowserRouter>
+
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import './style/globalStyle.scss';
+import { App } from './App';
+import React from 'react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
