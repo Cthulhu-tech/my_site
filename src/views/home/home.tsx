3a2e0092c73e09f0d16de3f44167ac6ff8df3dac
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-16 09:57:14 +0300

    update project

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,10 +1,9 @@
-import { Canvas } from '../../components/canvas/canvas';
 import styles from './homeStyle.module.scss';
 
 export const HomeView = () => {
 
     return <main className={styles.main}>
-        <Canvas/>
+
     </main>
 
 }
