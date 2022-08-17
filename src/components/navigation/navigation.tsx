164c81949d6932620283c56b7357a826018ba0bf
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-17 04:30:41 +0300

    navigation update

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,7 +4,10 @@ import './navigation.scss';
 export const Navigation = () => {
 
     return <nav className="navigation">
-            <div>
+        <div className="container__title">
+            <p className="navigation__title">Daniar M.</p>
+        </div>
+            <div className="container__links">
                 <NavLink to="/" className="link">HOME</NavLink>
                 <NavLink to="/project" className="link">PROJECT</NavLink>
                 <NavLink to="/contact" className="link">CONTACT</NavLink>
