3c564cea51d56ac1d9128e7f25d35c4221e7d8bb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-17 04:31:01 +0300

    layout add

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -1,4 +1,5 @@
 import { Navigation } from "./navigation/navigation";
+import { Contacts } from "./contacts/contacts";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./footer/footer";
 
@@ -7,6 +8,7 @@ export const Layouts = () => {
     return <>
     <Navigation/>
     <div className="background_image"/>
+    <Contacts/>
         <main className="main">
             <Outlet/>
         </main>
